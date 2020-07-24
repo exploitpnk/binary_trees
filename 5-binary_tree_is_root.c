088c1539db136f2caf66ed2cadc40a0aa693de4b
exploitpnk <exploitpnk@gmail.com>
2020-07-24 01:53:43 -0500

    [Add] test root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - test if node is root
+ * @node: ptr to node to be tested
+ *
+ * Return: 1 if node is root
+ **/
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+		return (0);
+	}
+
+	if ((node->parent) == NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
