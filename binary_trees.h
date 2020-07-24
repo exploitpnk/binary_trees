6908f99d1707d82c4cbefddc0fc92a17ad5c5546
exploitpnk <exploitpnk@gmail.com>
2020-07-24 04:53:17 -0500

    [Update] headers

--- binary_trees.h
+++ binary_trees.h
@@ -44,4 +44,8 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 size_t tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int left_depth(const binary_tree_t *node);
+int perfect_tree(const binary_tree_t *node, int depth, int lvl);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif /* _BINARY_TREES_H_ */
