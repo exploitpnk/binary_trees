b5c6ad0cff4fb34acc07045c72dbccfac97aa668
exploitpnk <exploitpnk@gmail.com>
2020-07-24 04:43:44 -0500

    [Add] perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,35 @@
 #include "binary_trees.h"
 /**
+ * perfect_tree - test if tree is full
+ * @node: node to check
+ * @lvl: init level
+ * @depth: depth
+ *
+ * Return: 1 if full
+ **/
+int perfect_tree(const binary_tree_t *node, int depth, int lvl)
+{
+	size_t left;
+	size_t right;
+
+	if (!node)
+	{
+		return (0);
+	}
+	if (!(node->left) && (!node->right))
+	{
+		return (depth == lvl + 1);
+	}
+	if (!(node->left) || (!node->right))
+	{
+		return (0);
+	}
+
+	left = perfect_tree(node->left, depth, lvl + 1);
+	right = perfect_tree(node->right, depth, lvl + 1);
+	return (left && right);
+}
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: ptr to root node
  * Return: 1 perfect, 0 NULL
@@ -30,34 +60,4 @@ int left_depth(const binary_tree_t *node)
 		node = node->left;
 	}
 	return (depth);
-}
-/**
- * perfect_tree - test if tree is full
- * @node: node to check
- * @lvl: init level
- * @depth: depth
- *
- * Return: 1 if full
- **/
-int perfect_tree(const binary_tree_t *node, int depth, int lvl)
-{
-	size_t left;
-	size_t right;
-
-	if (!node)
-	{
-		return (0);
-	}
-	if (!(node->left) && (!node->right))
-	{
-		return (depth == lvl + 1);
-	}
-	if (!(node->left) || (!node->right))
-	{
-		return (0);
-	}
-
-	left = perfect_tree(node->left, depth, lvl + 1);
-	right = perfect_tree(node->right, depth, lvl + 1);
-	return (left && right);
 }
