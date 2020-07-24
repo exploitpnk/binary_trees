c0c31de5f778443138fc1d8c9a0e9b6c556fe411
exploitpnk <exploitpnk@gmail.com>
2020-07-24 04:52:13 -0500

    [Add] sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: ptr to node
+ *
+ * Return: ptr to sibling
+ **/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *root;
+
+	root = NULL;
+
+	if (node == NULL || (node->parent) == NULL)
+	{
+		return (NULL);
+	}
+
+	root = node->parent;
+
+	if (root->left && (root->left != node))
+	{
+		return (root->left);
+	}
+	else if (root->right && (root->right != node))
+	{
+		return (root->right);
+	}
+	return (NULL);
+}
