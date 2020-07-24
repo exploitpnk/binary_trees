4573c97e9f62edf65a3e4ec3e81e73c9f0e94cce
exploitpnk <exploitpnk@gmail.com>
2020-07-24 03:22:36 -0500

    [Add] tree size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,5 +1,36 @@
 #include "binary_trees.h"
 /**
+ * height_add - heights add of binary trees
+ * @tree: ptr to root
+ *
+ * Return: height
+ **/
+size_t height_add(const binary_tree_t *tree)
+{
+	size_t heightadd = 0;
+	size_t height_l = 0;
+	size_t height_r = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left)
+	{
+		height_l = height_add(tree->left) + 1;
+	}
+
+	if (tree->right)
+	{
+		height_r = height_add(tree->right) + 1;
+	}
+
+	heightadd = height_l + height_r;
+
+	return (heightadd);
+}
+/**
  * binary_tree_size - get size of tree
  * @tree: ptr to node
  *
